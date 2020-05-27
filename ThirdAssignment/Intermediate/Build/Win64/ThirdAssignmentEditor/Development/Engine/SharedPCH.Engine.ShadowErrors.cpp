8f88e3414405d96aff2bf58f50c96320c9c810d5
Skyway666 <lucasgarciamateu@gmail.com>
2020-05-27 02:31:40 +0200

    Revert "Fuera gitigpollas"
    This reverts commit 2c77e5683b8c2e6348a5f99ad92bfce5d20cf22c.

--- ThirdAssignment/Intermediate/Build/Win64/ThirdAssignmentEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Gaming/Documents/GitHub/LD-Third-Assignment/ThirdAssignment/Intermediate/Build/Win64/ThirdAssignmentEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- ThirdAssignment/Intermediate/Build/Win64/ThirdAssignmentEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Gaming/Documents/Unreal Projects/ThirdAssignment/Intermediate/Build/Win64/ThirdAssignmentEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
