b4d7c05aab4d932d6bab80f5dc633e4ce079372b
FurryGhoul <ivandrofiak@hotmail.com>
2020-05-27 16:51:14 +0200

    New project pls work ffs

--- ThirdAssignment/Source/ThirdAssignment/ThirdAssignment.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ThirdAssignment.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThirdAssignment, "ThirdAssignment" );
- 
